b9f7af05ab34f18e60691ad93f755009836b3df1
horisaki <seiji.horisaki@gmail.com>
2018-12-25 15:33:21 +0900

    コマンド存在確認関数追加

--- export.sh
+++ export.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 DOT_DIRECTORY="${HOME}/dotfiles"
 
+has() {
+  type "$1" > /dev/null 2>&1
+}
+
 if has 'apm'; then
   apm list --installed --bare > ${DOT_DIRECTORY}/atom/packages.list
 fi
