e9afe1a1fdd299dd447f5ed9ee652570aab585ce
horisaki <seiji.horisaki@gmail.com>
2019-06-26 14:14:25 +0900

    move mkdir

--- .config/fish/functions/install_fisher.fish
+++ .config/fish/functions/install_fisher.fish
@@ -3,9 +3,10 @@
 if not functions -q fisher
   set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
   curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-  fish -c fisher
-end
 
-if not [ -d $XDG_CONFIG_HOME/fish/fisher-pkg ]
-  mkdir $XDG_CONFIG_HOME/fish/fisher-pkg
+  if not [ -d $XDG_CONFIG_HOME/fish/fisher-pkg ]
+    mkdir $XDG_CONFIG_HOME/fish/fisher-pkg
+  end
+
+  fish -c fisher
 end
