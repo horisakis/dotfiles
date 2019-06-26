603e3abab795c0c23a98fc9531783e5d3f2738fe
horisaki <seiji.horisaki@gmail.com>
2019-06-26 10:22:02 +0900

    use fisher

---
+++ .config/fish/functions/install_fisher.fish
@@ -0,0 +1,11 @@
+#!/usr/local/bin/fish
+
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
+if not [ -d $XDG_CONFIG_HOME/fish/fisher-pkg ]
+  mkdir $XDG_CONFIG_HOME/fish/fisher-pkg
+end
