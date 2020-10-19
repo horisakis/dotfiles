feb35ee66a5fc4493f69296b53829f53c52331a2
horisakis <seiji.horisaki@gmail.com>
2020-10-19 11:00:04 +0900

    事前に絞り込んでから実行できるよう変更

--- .config/fish/functions/cdghq.fish
+++ .config/fish/functions/cdghq.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
 function cdghq --wraps='cd (ghq root)/(ghq list | fzf)' --description 'alias cdghq cd (ghq root)/(ghq list | fzf)'
-  cd (ghq root)/(ghq list | fzf) $argv;
+  cd (ghq root)/(ghq list $argv| fzf);
 end
